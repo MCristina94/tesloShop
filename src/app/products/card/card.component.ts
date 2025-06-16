39f5e96568e0cc0aa01f1bf4cc02a68ee4d99c0f
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-16 17:31:16 -0500

    advances

---
+++ src/app/products/card/card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'product-card',
+  imports: [RouterModule],
+  templateUrl: './card.component.html',
+})
+export class CardComponent {}
--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CardComponent } from '@products/card/card.component';
+import { ProductsService } from '@products/services/product.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    // 'request' se renombra a 'params'
+    params: () => ({}),
+
+    // 'loader' se renombra a 'stream'.
+    // Ya no recibe un objeto { request }, sino directamente los parámetros.
+    // Como no hay parámetros, la función no necesita argumentos.
+    stream: () => {
+      return this.productsService.getProducts();
+    },
+  });
+}
