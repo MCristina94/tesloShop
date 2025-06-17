198203c3b6d20a70c49e30138b970d3209eb9c7d
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-17 10:49:37 -0500

    modified styles

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/product.service';
+import { map } from 'rxjs';
+import { CardComponent } from '../../../products/components/card/card.component';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender });
+    },
+  });
+}
