7c1cb13b37e75ac48d50689a2ec68bb1f91abffa
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-16 19:16:57 -0500

    advances

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/product.service';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+  productIdSlug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    params: () => ({ idSlug: this.productIdSlug }),
+    stream: ({ params }) => {
+      return this.productService.getProductById(params.idSlug);
+    },
+  });
+}
