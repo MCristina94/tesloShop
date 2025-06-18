a758672aefeda2541e2f25464bc76be1aae01cb9
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-17 21:25:46 -0500

    added information products on table

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductTableComponent } from '../../../products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/product.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTableComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsResource = rxResource({
+    // 'request' se renombra a 'params'
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
+
+    // 'loader' se renombra a 'stream'.
+    // Ya no recibe un objeto { request }, sino directamente los parámetros.
+    // Como no hay parámetros, la función no necesita argumentos.
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+      });
+    },
+  });
+}
