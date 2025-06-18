a6dcbb032b7ac41c980a81119d329eb9cca44946
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-17 19:39:55 -0500

    added guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [
+      //este es el guard para verificar acceso de usuario autorizado
+      //en este caso, si el usuario esta autenticado, no puede volver a estas rutas
+      NotAuthenticatedGuard,
+    ],
   },
   { path: '', loadChildren: () => import('./store-front/store-front.routes') },
 ];
