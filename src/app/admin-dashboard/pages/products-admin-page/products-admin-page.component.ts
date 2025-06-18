67f27e29b5d12f634c4b3cceabff8777afd67667
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-17 21:35:13 -0500

    advances

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,21 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductTableComponent } from '../../../products/components/product-table/product-table.component';
 import { ProductsService } from '@products/services/product.service';
 import { PaginationService } from '@shared/components/pagination/pagination.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTableComponent],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
 export class ProductsAdminPageComponent {
   productsService = inject(ProductsService);
   paginationService = inject(PaginationService);
 
+  productsPerPage = signal(10);
+
   productsResource = rxResource({
     // 'request' se renombra a 'params'
-    params: () => ({ page: this.paginationService.currentPage() - 1 }),
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }),
 
     // 'loader' se renombra a 'stream'.
     // Ya no recibe un objeto { request }, sino directamente los parámetros.
@@ -23,6 +29,7 @@ export class ProductsAdminPageComponent {
     stream: ({ params }) => {
       return this.productsService.getProducts({
         offset: params.page * 9,
+        limit: params.limit,
       });
     },
   });
