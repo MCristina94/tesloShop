b9491df700e0b149dc898552cb31b6ab573887f6
Maureen Garcia <cmaureen1908@gmail.com>
2025-06-17 22:52:06 -0500

    advances

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -3,10 +3,15 @@ import { Product } from '@products/interfaces/product.interface';
 import { ProductCarouselComponent } from '../../../../products/components/product-carousel/product-carousel.component';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '@utils/form-utils';
+import { FormErrorLabelComponent } from '../../../../shared/form-error-label/form-error-label.component';
 
 @Component({
   selector: 'app-product-details',
-  imports: [ProductCarouselComponent, ReactiveFormsModule],
+  imports: [
+    ProductCarouselComponent,
+    ReactiveFormsModule,
+    FormErrorLabelComponent,
+  ],
   templateUrl: './product-details.component.html',
 })
 export class ProductDetailsComponent implements OnInit {
